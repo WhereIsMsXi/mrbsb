df2efddcdad65d3dbf1d7bdb155ec23cb1627272
WhereIsMsXi <whereismsxi@gmail.com>
2024-07-03 10:24:01 +0800

    feat: 完成邮箱发送模块和发送邮件接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { User } from './user/entity/user.entity';
 import { Role } from './user/entity/role.entity';
 import { Permission } from './user/entity/permission.entity';
 import { RedisModule } from './redis/redis.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { RedisModule } from './redis/redis.module';
     }),
     UserModule,
     RedisModule,
+    EmailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register.dto';
+import { EmailService } from 'src/email/email.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Controller('user')
 export class UserController {
+  @Inject(EmailService)
+  private emailService: EmailService;
+
+  @Inject(RedisService)
+  private redisService: RedisService;
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
   register(@Body() registerUser: RegisterUserDto) {
     return this.userService.register(registerUser);
+  }
+
+  @Get('register-captcha')
+  async captcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(`captcha_${address}`, code, 5 * 60);
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '注册验证码',
+      html: `<div>
+        <p>您好，欢迎注册，</p>
+        <p>您的注册验证码是 ${code}</p>
+      </div>`,
+    });
+    return '发送成功';
   }
 }

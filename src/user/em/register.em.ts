973319cdd0205a69bc8e8bc97f0ee579dfe7787c
WhereIsMsXi <whereismsxi@gmail.com>
2024-07-03 10:28:45 +0800

    refactor: 优化注册接口的 service

---
+++ src/user/em/register.em.ts
@@ -0,0 +1,28 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { User } from '../entity/user.entity';
+import { md5 } from 'src/utils/utils';
+
+export function validateCaptcha(captcha: string, redisCaptcha: string) {
+  if (!captcha) {
+    throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+  }
+
+  if (captcha !== redisCaptcha) {
+    throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export function validateRegisterUser(user: User) {
+  if (user) {
+    throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export function createUser(user: User) {
+  const result = new User();
+  result.username = user.username;
+  result.password = md5(user.password);
+  result.email = user.email;
+  result.nickName = user.nickName;
+  return result;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,14 @@
-import {
-  HttpException,
-  HttpStatus,
-  Inject,
-  Injectable,
-  Logger,
-} from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
 import { RegisterUserDto } from './dto/register.dto';
 import { RedisService } from 'src/redis/redis.service';
-import { md5 } from 'src/utils/utils';
+import {
+  createUser,
+  validateCaptcha,
+  validateRegisterUser,
+} from './em/register.em';
 
 @Injectable()
 export class UserService {
@@ -24,28 +22,14 @@ export class UserService {
 
   async register(user: RegisterUserDto) {
     const captcha = await this.redisService.get(`captcha_${user.email}`);
-
-    if (!captcha) {
-      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
-    }
-
-    if (user.captcha !== captcha) {
-      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
-    }
+    validateCaptcha(captcha, user.captcha);
 
     const foundUser = await this.userRepository.findOneBy({
       username: user.username,
     });
+    validateRegisterUser(foundUser);
 
-    if (foundUser) {
-      throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST);
-    }
-
-    const newUser = new User();
-    newUser.username = user.username;
-    newUser.password = md5(user.password);
-    newUser.email = user.email;
-    newUser.nickName = user.nickName;
+    const newUser = await createUser(foundUser);
 
     try {
       await this.userRepository.save(newUser);

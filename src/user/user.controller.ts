e3d9f4943438ca860b2a8c8ad488a4f972675bf2
WhereIsMsXi <whereismsxi@gmail.com>
2024-07-03 09:34:17 +0800

    feat: init User 的 注册接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {
+    return this.userService.register(registerUser);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { RegisterUserDto } from './dto/register.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private logger = new Logger();
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+
+  async register(user: RegisterUserDto) {
+    console.log(user);
+    return 'success';
+  }
+}

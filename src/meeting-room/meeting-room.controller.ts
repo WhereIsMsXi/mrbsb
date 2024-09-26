1d6287b0256a537b9ae8f3c4196a19b1d52786b8
WhereIsMsXi <whereismsxi@gmail.com>
2024-09-26 17:42:36 +0800

    feat: 会议室接口文档

--- src/meeting-room/meeting-room.controller.ts
+++ src/meeting-room/meeting-room.controller.ts
@@ -4,6 +4,7 @@ import {
   DefaultValuePipe,
   // Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Put,
@@ -13,12 +14,54 @@ import { MeetingRoomService } from './meeting-room.service';
 import { generateParseIntPipe } from 'src/utils/utils';
 import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
 import { UpdateMeetingRoomDto } from './dto/update-meeting-room.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { RequireLogin } from 'src/custom.decorater';
+import { MeetingRoomVo } from './vo/meeting-room.vo';
+import { MeetingRoomListVo } from './vo/meeting-room-list.vo';
 
+@ApiTags('会议室模块')
 @Controller('meeting-room')
 export class MeetingRoomController {
   constructor(private readonly meetingRoomService: MeetingRoomService) {}
 
   @Get('list')
+  @ApiBearerAuth()
+  @RequireLogin()
+  @ApiQuery({
+    name: 'pageNo',
+    type: Number,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'pageSize',
+    type: Number,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'name',
+    type: String,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'capacity',
+    type: String,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'equipment',
+    type: String,
+    required: false,
+  })
+  @ApiResponse({
+    type: MeetingRoomListVo,
+  })
   async list(
     @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo'))
     pageNo: number,
@@ -42,21 +85,69 @@ export class MeetingRoomController {
   }
 
   @Get(':id')
+  @ApiBearerAuth()
+  @RequireLogin()
+  @ApiParam({
+    name: 'id',
+    type: Number,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'success',
+    type: MeetingRoomVo,
+  })
   async find(@Param('id') id: number) {
     return await this.meetingRoomService.findById(id);
   }
 
   @Post('create')
+  @ApiBearerAuth()
+  @RequireLogin()
+  @ApiBody({
+    type: CreateMeetingRoomDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '会议室名字已存在',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: MeetingRoomVo,
+  })
   async create(@Body() meetingRoomDto: CreateMeetingRoomDto) {
     return await this.meetingRoomService.create(meetingRoomDto);
   }
 
   @Put('update')
+  @ApiBearerAuth()
+  @RequireLogin()
+  @ApiBody({
+    type: UpdateMeetingRoomDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '会议室不存在',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'success',
+  })
   async update(@Body() meetingRoomDto: UpdateMeetingRoomDto) {
     return await this.meetingRoomService.update(meetingRoomDto);
   }
 
   // @Delete(':id')
+  // @ApiBearerAuth()
+  // @RequireLogin()
+  // @ApiParam({
+  //   name: 'id',
+  //   type: Number,
+  //   description: 'id'
+  // })
+  // @ApiResponse({
+  //   status: HttpStatus.OK,
+  //   description: 'success'
+  // })
   // async delete(@Param('id') id: number) {
   //   return await this.meetingRoomService.delete(id);
   // }
